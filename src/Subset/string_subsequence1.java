f353ef59980cc4239096cdccd8299bd322454073
Mayankdaiya <mayankdaiya03@gmail.com>
2022-12-13 12:32:02 +0530

    Recursion

---
+++ src/Subset/string_subsequence1.java
@@ -0,0 +1,19 @@
+package Subset;
+import java.util.Scanner;
+
+public class string_subsequence1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        printSubsequence(str,"");
+    }
+    public  static void printSubsequence(String str,String ans){
+        if(str.isEmpty()){
+            System.out.println(ans);
+            return;
+        }
+        char ch=str.charAt(0);
+        printSubsequence(str.substring(1),ans+ch);
+        printSubsequence(str.substring(1),ans);
+    }
+}
