624515d9b8115c894829ab8ea2ce6affe732907b
Mayankdaiya <mayankdaiya03@gmail.com>
2022-12-21 18:49:05 +0530

    Recursion

--- src/Subset/string_subsequence1.java
+++ src/Subset/string_subsequence1.java
@@ -1,41 +1,19 @@
 package Subset;
-//import java.util.Scanner;
-//
-//public class string_subsequence1 {
-//    public static void main(String[] args) {
-//        Scanner sc=new Scanner(System.in);
-//        String str=sc.next();
-//        printSubsequence(str,"");
-//    }
-//    public  static void printSubsequence(String str,String ans){
-//        if(str.isEmpty()){
-//            System.out.println(ans);
-//            return;
-//        }
-//        char ch=str.charAt(0);
-//        printSubsequence(str.substring(1),ans+ch);
-//        printSubsequence(str.substring(1),ans);
-//    }
-//}
+import java.util.Scanner;
 
-import java.util.*;
-public class Main {
-    public static void main(String args[]) {
+public class string_subsequence1 {
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        Eratosth(n);
+        String str=sc.next();
+        printSubsequence(str,"");
     }
-    public static void Eratosth(int n){
-        int num=1, c=0, i;
-        while(c<n){
-            num++;
-            for(i=2;i<=num;i++){
-                if(num%i==0) break;
-            }
-            if(i==num){
-                c++;
-            }
+    public  static void printSubsequence(String str,String ans){
+        if(str.isEmpty()){
+            System.out.println(ans);
+            return;
         }
-        System.out.println(num);
+        char ch=str.charAt(0);
+        printSubsequence(str.substring(1),ans+ch);
+        printSubsequence(str.substring(1),ans);
     }
 }
