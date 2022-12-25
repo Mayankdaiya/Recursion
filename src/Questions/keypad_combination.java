5997a50e514bc328d78f1c2c3cdf272724375905
Mayankdaiya <mayankdaiya03@gmail.com>
2022-12-25 21:08:38 +0530

    Recursion

---
+++ src/Questions/keypad_combination.java
@@ -0,0 +1,22 @@
+package Questions;
+
+import java.util.Scanner;
+
+public class keypad_combination {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str= sc.next();
+        getTheCombination(str,"");
+    }
+    public static void getTheCombination(String str1,String str2){
+        if(str1.isEmpty()){
+            System.out.println(str2);
+            return;
+        }
+        int digit=str1.charAt(0)-'0';
+        for(int i=(digit-1)*3;i<digit*3;i++){
+            char ch=(char)('a'+i);
+            getTheCombination(str1.substring(1),str2+ch);
+        }
+    }
+}
