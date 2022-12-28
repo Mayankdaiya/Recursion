5161e485aab7c27995abdf6615cb86a11bab17ca
Mayankdaiya <mayankdaiya03@gmail.com>
2022-12-28 19:39:10 +0530

    Recursion

---
+++ src/Questions/target_no_combination3.java
@@ -0,0 +1,22 @@
+package Questions;
+
+import java.util.Scanner;
+
+public class target_no_combination3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println("\n"+getTheCombination(num,""));
+    }
+    public static int getTheCombination(int num,String ans){
+        if(num==0){
+            System.out.print(ans+" ");
+            return 1;
+        }
+        int count=0;
+        for(int i=1;i<=num;i++){
+            if(num-i>=0) count+=getTheCombination(num-i,ans+i);
+        }
+        return count;
+    }
+}
