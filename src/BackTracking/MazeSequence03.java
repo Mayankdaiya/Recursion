1fa34957ab9c57406e60c04656be7653ec0949a8
Mayankdaiya <mayankdaiya03@gmail.com>
2023-01-05 10:53:20 +0530

    Recursion

--- src/BackTracking/MazeSequence03.java
+++ src/BackTracking/MazeSequence03.java
@@ -7,20 +7,25 @@ public class MazeSequence03 {
         Scanner sc=new Scanner(System.in);
         boolean grid[][]={
                 {true,true,true},
-                {true,false,true},
+                {true,true,true},
                 {true,true,true}
         };
-        System.out.println("\n"+printSubsequence(grid,0,0,""));
+        printSubsequence(grid,0,0,"");
     }
-    public static int printSubsequence(boolean[][] board, int i,int j,String ans){
+    public static void printSubsequence(boolean[][] board, int i,int j,String ans){
         if(i==board.length-1 && j==board[0].length-1){
             System.out.print(ans+" ");
-            return 1;
+            return;
         }
-        if(!board[i][j]) return 0;
-        int count=0;   //for counting the sequence
-        if(i<board.length-1) count+=printSubsequence(board,i+1,j,ans+'D');   // D for down movement in grid
-        if(j<board[0].length-1) count+=printSubsequence(board,i,j+1,ans+'R');   // R for right movement in grid
-        return count;
+        if(!board[i][j]) return;
+        board[i][j]=false;
+        if(j<board[0].length-1) printSubsequence(board,i,j+1,ans+'R');   // R for right movement in grid
+        if(i<board.length-1) printSubsequence(board,i+1,j,ans+'D');   // D for down movement in grid
+        if(j>0) printSubsequence(board,i,j-1,ans+'L');   // L for left movement in grid
+        if(i>0) printSubsequence(board,i-1,j,ans+'U');   // U for up movement in grid
+
+        //this line is where the functions will be over
+        //so before the functions gets removed, also removed the changes that were made by the function
+        board[i][j]=true;
     }
 }
