8e46a743b0ba495cf0cc1b52f4c0bb62619d67dd
Mayankdaiya <mayankdaiya03@gmail.com>
2023-01-07 19:06:23 +0530

    Recursion

--- src/BackTracking/MazeSequence01.java
+++ src/BackTracking/MazeSequence01.java
@@ -2,7 +2,7 @@ package BackTracking;
 
 import java.util.Scanner;
 
-public class MazeSequence {
+public class MazeSequence01 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int grid=sc.nextInt();
