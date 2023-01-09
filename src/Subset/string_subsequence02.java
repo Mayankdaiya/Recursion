f2544b4867dcfe4bfbe62c50043c29f6e6b6268d
Mayankdaiya <mayankdaiya03@gmail.com>
2023-01-09 23:20:25 +0530

    Recursion

--- src/Subset/string_subsequence02.java
+++ src/Subset/string_subsequence02.java
@@ -3,7 +3,7 @@ package Subset;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class string_subsequence2 {
+public class string_subsequence02 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String str=sc.next();
